379e51800c0de6bd35fbdd1915b876f23e687732
Vurv <56230599+Vurv78@users.noreply.github.com>
2020-09-27 22:50:29 -0700

    Update cl_coroutines.lua

--- lua/entities/gmod_wire_expression2/core/custom/cl_coroutines.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cl_coroutines.lua
@@ -7,10 +7,10 @@ desc("coroutine(s)","Creates a coroutine object to be run with xco:resume()")
 desc("coroutineRunning()","Returns the current e2 coroutine running, else nothing.")
 
 -- Metamethods
-desc("xco:status()","Returns a string of the status of the coroutine, 'dead' for finished, 'suspended' for yielded, and 'running' for obvious reasons")
-desc("xco:wait(n)","Makes a coroutine wait for n amount of seconds, in this time, it is yielded and cannot be resumed.")
-desc("xco:yield()","Makes the coroutine pause until it is resumed again. It will remember everything that is happening")
-desc("xco:reboot()","Returns a coroutine object that behaves as if the coroutine given was never started or was reset, 'rebooting' it.")
+desc("status(xco:)","Returns a string of the status of the coroutine, 'dead' for finished, 'suspended' for yielded, and 'running' for obvious reasons")
+desc("wait(xco:n)","Makes a coroutine wait for n amount of seconds, in this time, it is yielded and cannot be resumed.")
+desc("yield(xco:)","Makes the coroutine pause until it is resumed again. It will remember everything that is happening")
+desc("reboot(xco:)","Returns a coroutine object that behaves as if the coroutine given was never started or was reset, 'rebooting' it.")
 
 -- Try & Catch ( Kind of separate )
 
