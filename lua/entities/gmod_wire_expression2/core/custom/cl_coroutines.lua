1411979a2866b4f110d95e20f111672f9fa99be7
Vurv78 <Vurv78@users.noreply.github.com>
2020-11-12 21:48:29 -0800

    Overhaul of CoroutineCore
    Yay! it actually works now! I don't have to do anything anymore!
    (have)
    Also adds a failsafe for if someone already has vextensions installed and has this addon already for whatever reason.

--- lua/entities/gmod_wire_expression2/core/custom/cl_coroutines.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cl_coroutines.lua
@@ -4,7 +4,15 @@ local function desc(name,desc)
 end
 
 desc("coroutine(s)","Creates a coroutine object to be run with xco:resume()")
-desc("coroutineRunning()","Returns the current e2 coroutine running, else nothing.")
+desc("coroutineRunning()","Returns the current e2 coroutine running, else nothing")
+desc("coroutineYield()","Makes the coroutine pause until it is resumed again. It will remember everything that is happening")
+desc("coroutineWait(n)","Makes a coroutine wait for n amount of seconds, in this time, it is yielded and cannot be resumed")
+
+-- Metamethods
+desc("status(xco:)","Returns a string of the status of the coroutine, 'dead' for finished, 'suspended' for yielded, and 'running' for obvious reasons")
+desc("wait(xco:n)","Makes a coroutine wait for n amount of seconds, in this time, it is yielded and cannot be resumed")
+desc("yield(xco:)","Makes the coroutine pause until it is resumed again. It will remember everything that is happening")
+desc("reboot(xco:)","Returns a coroutine object that behaves as if the coroutine given was never started or was reset, 'rebooting' it")
 
 -- Metamethods
 desc("status(xco:)","Returns a string of the status of the coroutine, 'dead' for finished, 'suspended' for yielded, and 'running' for obvious reasons")
